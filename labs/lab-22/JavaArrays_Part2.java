453de078923fdde409df009e5552b35140cfc06b
Ben Russell <pandaben@me.com>
2023-11-08 16:23:20 -0500

    java arrays lab part 2

--- labs/lab-22/JavaArrays_Part2.java
+++ labs/lab-22/JavaArrays_Part2.java
@@ -2,7 +2,7 @@
 
 import java.util.Arrays;
 import java.util.Scanner;
-
+import java.util.Random;
 /*
  * CPS 150
  * Algorithms & Programming I
@@ -17,5 +17,41 @@ public class JavaArrays_Part2
   public static void main(String[] args)
   {
   	// ADD LAB CODE HERE
+    Random gen = new Random();
+
+    double arr[] = new double[20];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = gen.nextDouble(99) + 1;
+      System.out.println(arr[i]);
+    }
+
+    double sum = 0;
+    for(double val : arr){
+      sum += val;
+    }
+
+    System.out.println("Sum: " + sum);
+
+    double min = arr[0];
+    for(double val : arr){
+      if(val < min){
+        min = val;
+      }
+    }
+
+    System.out.println("Minimum: " + min);
+
+    min = arr[0];
+    int minIndex = 0; 
+    for(int i = 0; i < arr.length; i++){
+      if(arr[i] < min){
+        min = arr[i];
+        minIndex = i;
+      }
+    }
+
+    System.out.println("Minimum: " + min + ", Index: " + minIndex);
+
+
   } // end main method
 } // end class JavaArrays_Part2
